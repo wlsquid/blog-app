0cc88855d459dd13504ff0853a5040b03c0d79dd
wlsquid <wyndhammorland@gmail.com>
2021-07-09 21:10:03 +1000

    wip

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,72 @@
 <footer class="bg-gray-800-py-20 mt-20">
-<div class="sm:grid grid:cols-3 w-25 pb-10 m-auto border-b-2 border-gray-700">
+<div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
   <div>
-  
+    <h3 class="text-l sm:font-bold text-gray-100">
+    pages
+    </h3>
+    <ul class="py-4 sm:text-s pt-4 text-gray-400">
+      <li class="pb-1">
+           <a href="/">
+          Home
+          </a>
+        </li>
+        <li>
+          <a href="/blog">
+            Blog
+            </a>
+            <a href="/login">
+              Login
+              </a>
+              <a href="/register">
+                Register
+                </a>
+      </li>
+    </ul>
+  </div>
+  <div>
+    <h3 class="text-l sm:font-bold text-gray-100">
+    Find Us
+    </h3>
+    <ul class="py-4 sm:text-s pt-4 text-gray-400">
+      <li class="pb-1">
+           <a href="/">
+          What We Do
+          </a>
+          <a href="/">
+            Phone
+            </a>
+            <a href="/">
+              Address
+              </a>
+              <a href="/contact">
+                Contact 
+                </a>
+      </li>
+    </ul>
+  </div>
+  <div>
+    <h3 class="text-l sm:font-bold text-gray-100">
+    Latest Posts
+    </h3>
+    <ul class="py-4 sm:text-s pt-4 text-gray-400">
+      <li class="pb-1">
+           <a href="/">
+          Why tech is inherently manegerial
+          </a>
+          <a href="/">
+            IT cannot solve all problems
+            </a>
+            <a href="/">
+              Unnecessary
+              </a>
+              <a href="/">
+                blog
+                </a>
+      </li>
+    </ul>
   </div>
 </div>
+<p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+  Copyright 2021 Wimba Industries
+</p>
 </footer>
